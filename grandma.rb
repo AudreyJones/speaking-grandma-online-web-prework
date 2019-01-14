b209f5013e32e9bcefaadcd95464e61eedd19a69
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-01-14 16:07:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma
-  if (phrase == )
-    return "HUH?! SPEAK UP, SONNY!"
+  if (phrase.upcase)
+    return "NO, NOT SINCE 1938!"
   elsif (phrase == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
   else
-    return "NO, NOT SINCE 1938!"
+    return "HUH?! SPEAK UP, SONNY!"
 end
